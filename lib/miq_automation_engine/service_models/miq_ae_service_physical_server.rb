00932aadff3b28173146dd2e2510ca0630b74473
Andrey Menezes <andrey.menezes@gmail.com>
2017-03-10 00:02:05 -0300

    Create service to led and power manipulation

---
+++ lib/miq_automation_engine/service_models/miq_ae_service_physical_server.rb
@@ -0,0 +1,25 @@
+module MiqAeMethodService
+  class MiqAeServicePhysicalServer < MiqAeServiceModelBase
+  	expose :ext_management_system, :association => true
+  	expose :physical_servers,      :association => true
+  	#expose :to_s
+
+
+  	def turn_on_loc_led
+  		object_send(:turn_on_loc_led)
+  	end
+
+  	def turn_off_loc_led
+  		object_send(:turn_off_loc_led)
+  	end
+
+  	def power_on
+  		object_send(:power_on)
+  	end
+
+  	def power_off
+  		object_send(:power_off)
+    end
+
+   end
+end
